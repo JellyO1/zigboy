853bbae72f2e42f3ae2a1bb6727bf91970e66279
Daniel Evangelista <danivangelis@hotmail.com>
2025-07-25 20:46:57 +0100

    feat: configure imgui layout

--- src/graphics.zig
+++ src/graphics.zig
@@ -8,6 +8,10 @@ const c = @cImport({
     // macro magic, 'SDL_MAIN_HANDLED' should be defined before including 'SDL_main.h'.
     @cDefine("SDL_MAIN_HANDLED", {});
     @cInclude("SDL3/SDL_main.h");
+
+    @cInclude("dcimgui.h");
+    @cInclude("backends/dcimgui_impl_sdl3.h");
+    @cInclude("backends/dcimgui_impl_sdlrenderer3.h");
 });
 
 pub fn init() !void {
@@ -15,26 +19,66 @@ pub fn init() !void {
     // macro magic, 'SDL_SetMainReady' should be called before calling 'SDL_Init'.
     c.SDL_SetMainReady();
 
-    _ = c.SDL_SetAppMetadata("ZigZagZog", "0.0.1", "com.zigzagzog.zigzagzog");
-    if (!c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO | c.SDL_INIT_GAMEPAD)) {
-        return error.SdlError;
-    }
+    _ = c.SDL_SetAppMetadata("ZigBoy", "0.0.1", "dev.tobedefined.zigboy");
+    try errify(c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO | c.SDL_INIT_GAMEPAD));
 }
 
 pub fn createWindow(title: []const u8, w: c_int, h: c_int) !struct { *c.SDL_Window, *c.SDL_Renderer } {
-    const window = if (c.SDL_CreateWindow(title.ptr, w, h, 0)) |l| l else {
-        return error.SdlError;
-    };
+    const scale = c.SDL_GetDisplayContentScale(c.SDL_GetPrimaryDisplay());
+    const window = try errify(c.SDL_CreateWindow(title.ptr, @intFromFloat(@as(f32, @floatFromInt(w)) * scale), @intFromFloat(@as(f32, @floatFromInt(h)) * scale), c.SDL_WINDOW_RESIZABLE | c.SDL_WINDOW_HIGH_PIXEL_DENSITY));
 
-    const renderer = if (c.SDL_CreateRenderer(window, null)) |r| r else {
-        return error.SdlError;
-    };
+    const renderer: *c.SDL_Renderer = try errify(c.SDL_CreateRenderer(window, null));
+
+    // setup IMGUI
+    // Setup Dear ImGui context
+    if (c.ImGui_CreateContext(null) == null) return error.ImGuiCreateContextFailure;
+
+    const io = c.ImGui_GetIO(); // (void)io;
+    io.*.ConfigFlags |= c.ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.*.ConfigFlags |= c.ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+    io.*.ConfigFlags |= c.ImGuiConfigFlags_DockingEnable; // Enable docking (NOTE: For some reason no ImGuiConfigFlags_EnableDocking is available)
+
+    // Setup Dear ImGui style
+    c.ImGui_StyleColorsDark(c.ImGui_GetStyle());
+
+    // Setup scaling
+    const style = c.ImGui_GetStyle();
+    c.ImGuiStyle_ScaleAllSizes(style, scale); // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+    style.*.FontScaleDpi = scale; // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
+
+    // Setup Platform/Renderer backends
+    try errify(c.cImGui_ImplSDL3_InitForSDLRenderer(window, renderer));
+    try errify(c.cImGui_ImplSDLRenderer3_Init(renderer));
 
     return .{ window, renderer };
 }
 
 pub fn destroyWindow(window: *c.SDL_Window, renderer: *c.SDL_Renderer) void {
     c.SDL_Quit();
+    c.cImGui_ImplSDLRenderer3_Shutdown();
+    c.cImGui_ImplSDL3_Shutdown();
+    c.ImGui_DestroyContext(null);
     c.SDL_DestroyRenderer(renderer);
     c.SDL_DestroyWindow(window);
+}
+
+/// Converts the return value of an SDL function to an error union.
+pub inline fn errify(value: anytype) error{SdlError}!switch (@typeInfo(@TypeOf(value))) {
+    .bool => void,
+    .pointer, .optional => @TypeOf(value.?),
+    .int => |info| switch (info.signedness) {
+        .signed => @TypeOf(@max(0, value)),
+        .unsigned => @TypeOf(value),
+    },
+    else => @compileError("unerrifiable type: " ++ @typeName(@TypeOf(value))),
+} {
+    return switch (@typeInfo(@TypeOf(value))) {
+        .bool => if (!value) error.SdlError,
+        .pointer, .optional => value orelse error.SdlError,
+        .int => |info| switch (info.signedness) {
+            .signed => if (value >= 0) @max(0, value) else error.SdlError,
+            .unsigned => if (value != 0) value else error.SdlError,
+        },
+        else => comptime unreachable,
+    };
 }
--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,11 @@ const c = @cImport({
     @cDefine("SDL_DISABLE_OLD_NAMEs", {});
     @cInclude("SDL3/SDL.h");
     @cInclude("SDL3/SDL_revision.h");
+
+    @cInclude("dcimgui.h");
+    @cInclude("dcimgui_internal.h");
+    @cInclude("backends/dcimgui_impl_sdl3.h");
+    @cInclude("backends/dcimgui_impl_sdlrenderer3.h");
 });
 
 const graphics = @import("graphics.zig");
@@ -2155,18 +2160,28 @@ pub fn debugTilemap(state: *GameBoyState, buffer: *[256 * 256]RGBA) void {
     }
 }
 
-pub fn main() !void {
+fn fitAspect(img_w: f32, img_h: f32) struct { w: f32, h: f32 } {
+    const avail = c.ImGui_GetContentRegionAvail();
+    const aspect = img_w / img_h;
+    var draw_w = avail.x;
+    var draw_h = avail.y;
+    if (draw_w / aspect > draw_h) {
+        draw_w = draw_h * aspect;
+    } else {
+        draw_h = draw_w / aspect;
+    }
+    return .{ .w = draw_w, .h = draw_h };
+}
 
-    // var gameBoyState: GameBoyState = try GameBoyState.init(null);
+pub fn main() !void {
+    var gameBoyState: GameBoyState = try GameBoyState.init(null);
 
     {
         errdefer |err| if (err == error.SdlError) std.log.err("SDL error: {s}", .{c.SDL_GetError()});
 
         try graphics.init();
 
-        const scale = 2;
-
-        const mainWindow, const mainRenderer = try graphics.createWindow(@as([]const u8, "ZigBoy"), (160 + 192 + 256) * scale, 256 * scale);
+        const mainWindow, const mainRenderer = try graphics.createWindow(@as([]const u8, "ZigBoy"), 1920, 1080);
         defer graphics.destroyWindow(mainWindow, mainRenderer);
         const framebufferTexture = c.SDL_CreateTexture(@ptrCast(mainRenderer), c.SDL_PIXELFORMAT_RGBA8888, c.SDL_TEXTUREACCESS_STREAMING, 160, 144);
         const tileTexture = c.SDL_CreateTexture(@ptrCast(mainRenderer), c.SDL_PIXELFORMAT_RGBA8888, c.SDL_TEXTUREACCESS_STREAMING, 192, 128);
@@ -2183,34 +2198,109 @@ pub fn main() !void {
 
             var ev: c.SDL_Event = undefined;
             while (c.SDL_PollEvent(&ev)) {
+                _ = c.cImGui_ImplSDL3_ProcessEvent(&ev);
                 if (ev.type == c.SDL_EVENT_QUIT) {
                     break :mainLoop;
                 }
             }
 
+            // Prepare for ImGui frame.
+            // Start the Dear ImGui frame
+            c.cImGui_ImplSDLRenderer3_NewFrame();
+            c.cImGui_ImplSDL3_NewFrame();
+            c.ImGui_NewFrame();
+
             gameBoyState.step();
-            std.debug.print("{}\n", .{gameBoyState.registers});
+            //std.debug.print("{}\n", .{gameBoyState.registers});
 
             // HACK: Only render and present at the end of the last VBlank scanline
             if (gameBoyState.ppu.mode == .VBlank and gameBoyState.ppu.scanline.* == 144) {
-                _ = c.SDL_RenderClear(@ptrCast(mainRenderer));
                 _ = c.SDL_UpdateTexture(framebufferTexture, null, &gameBoyState.ppu.framebuffer, 160 * @sizeOf(RGBA));
-                //std.log.info("{any}", .{gameBoyState.registers});
-                _ = c.SDL_RenderTexture(@ptrCast(mainRenderer), framebufferTexture, null, &c.SDL_FRect{ .x = 0, .y = 0, .w = 160 * scale, .h = 144 * scale });
 
                 _ = c.SDL_LockTexture(tileTexture, null, &tileBuffer, &tilePitch);
-                // debugTileData(&gameBoyState, @ptrCast(@alignCast(tileBuffer)));
                 debugTileset(&gameBoyState, @ptrCast(@alignCast(tileBuffer)));
                 _ = c.SDL_UnlockTexture(tileTexture);
-                _ = c.SDL_RenderTexture(@ptrCast(mainRenderer), tileTexture, null, &c.SDL_FRect{ .x = 160 * scale, .y = 0, .w = 192 * scale, .h = 144 * scale });
 
                 _ = c.SDL_LockTexture(tilemapTexture, null, &tilemapBuffer, &tilemapPitch);
                 debugTilemap(&gameBoyState, @ptrCast(@alignCast(tilemapBuffer)));
                 _ = c.SDL_UnlockTexture(tilemapTexture);
-                _ = c.SDL_RenderTexture(@ptrCast(mainRenderer), tilemapTexture, null, &c.SDL_FRect{ .x = (160 + 192) * scale, .y = 0, .w = 256 * scale, .h = 256 * scale });
-
-                _ = c.SDL_RenderPresent(@ptrCast(mainRenderer));
             }
+
+            var windowFlags: c_int = c.ImGuiWindowFlags_MenuBar | c.ImGuiWindowFlags_NoDocking;
+            const viewport = c.ImGui_GetMainViewport();
+            c.ImGui_SetNextWindowPos(viewport.*.Pos, 0);
+            c.ImGui_SetNextWindowSize(viewport.*.Size, 0);
+            c.ImGui_PushStyleVar(c.ImGuiStyleVar_WindowRounding, 0.0);
+            c.ImGui_PushStyleVar(c.ImGuiStyleVar_WindowBorderSize, 0.0);
+            c.ImGui_PushStyleVarImVec2(c.ImGuiStyleVar_WindowPadding, c.ImVec2{ .x = 0.0, .y = 0.0 });
+            windowFlags |= c.ImGuiWindowFlags_NoTitleBar | c.ImGuiWindowFlags_NoCollapse | c.ImGuiWindowFlags_NoResize | c.ImGuiWindowFlags_NoMove;
+            windowFlags |= c.ImGuiWindowFlags_NoBringToFrontOnFocus | c.ImGuiWindowFlags_NoNavFocus;
+
+            _ = c.ImGui_Begin("ZigBoy", @ptrCast(@constCast(&true)), windowFlags);
+            c.ImGui_PopStyleVarEx(3);
+
+            const dockspaceId = c.ImGui_GetID("MyDockSpace");
+            _ = c.ImGui_DockSpaceEx(dockspaceId, c.ImVec2{ .x = 0.0, .y = 0.0 }, 0, 0);
+
+            const S = struct {
+                var firstTime: bool = true;
+            };
+
+            if (S.firstTime) {
+                S.firstTime = false;
+
+                // c.ImGui_DockBuilderRemoveNode(dockspaceId); // clear any previous layout
+                // _ = c.ImGui_DockBuilderAddNodeEx(dockspaceId, c.ImGuiDockNodeFlags_DockSpace);
+                // c.ImGui_DockBuilderSetNodeSize(dockspaceId, viewport.*.Size);
+
+                // split the dockspace into 2 nodes -- DockBuilderSplitNode takes in the following args in the following order
+                //   window ID to split, direction, fraction (between 0 and 1), the final two setting let's us choose which id we want (which ever one we DON'T set as NULL, will be returned by the function)
+                //                                                              out_id_at_dir is the id of the node in the direction we specified earlier, out_id_at_opposite_dir is in the opposite direction
+                const dockIdRight = c.ImGui_DockBuilderSplitNode(dockspaceId, c.ImGuiDir_Left, 0.5, null, @ptrCast(@constCast(&dockspaceId)));
+                const dockIdLeft = c.ImGui_DockBuilderSplitNode(dockspaceId, c.ImGuiDir_Left, 0.5, null, @ptrCast(@constCast(&dockspaceId)));
+
+                // we now dock our windows into the docking node we made above
+                c.ImGui_DockBuilderDockWindow("Display", dockIdLeft);
+                c.ImGui_DockBuilderDockWindow("Debug", dockIdRight);
+                c.ImGui_DockBuilderFinish(dockspaceId);
+            }
+
+            c.ImGui_End();
+
+            _ = c.ImGui_Begin("Display", null, 0);
+            var fit = fitAspect(@floatFromInt(framebufferTexture.*.w), @floatFromInt(framebufferTexture.*.h));
+            c.ImGui_Image(c.ImTextureRef{ ._TexID = @intFromPtr(framebufferTexture) }, c.ImVec2{ .x = fit.w, .y = fit.h });
+            c.ImGui_End();
+
+            // Memory TabBar
+            _ = c.ImGui_Begin("Debug", null, 0);
+            if (c.ImGui_BeginTabBar("DebugTabBar", 0)) {
+                // Tile texture
+                if (c.ImGui_BeginTabItem("VRAM", null, 0)) {
+                    fit = fitAspect(@floatFromInt(tileTexture.*.w), @floatFromInt(tileTexture.*.h));
+                    c.ImGui_Image(c.ImTextureRef{ ._TexID = @intFromPtr(tileTexture) }, c.ImVec2{ .x = fit.w, .y = fit.h });
+                    c.ImGui_EndTabItem();
+                }
+
+                // Tilemap texture
+                if (c.ImGui_BeginTabItem("Tilemap", null, 0)) {
+                    fit = fitAspect(@floatFromInt(tilemapTexture.*.w), @floatFromInt(tilemapTexture.*.h));
+
+                    c.ImGui_Image(c.ImTextureRef{ ._TexID = @intFromPtr(tilemapTexture) }, c.ImVec2{ .x = fit.w, .y = fit.h });
+                    c.ImGui_EndTabItem();
+                }
+
+                c.ImGui_EndTabBar();
+            }
+            c.ImGui_End();
+
+            c.ImGui_Render();
+            const drawData: *c.ImDrawData = c.ImGui_GetDrawData();
+            const io = c.ImGui_GetIO();
+            _ = c.SDL_SetRenderScale(@ptrCast(mainRenderer), io.*.DisplayFramebufferScale.x, io.*.DisplayFramebufferScale.y);
+            _ = c.SDL_RenderClear(@ptrCast(mainRenderer));
+            c.cImGui_ImplSDLRenderer3_RenderDrawData(drawData, @ptrCast(mainRenderer));
+            _ = c.SDL_RenderPresent(@ptrCast(mainRenderer));
         }
     }
 }
